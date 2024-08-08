9e6d1a9fa9355415fbaba5c0ca2858cebdccd7b2
Adam Robson <adamrayrobson@gmail.com>
2024-08-07 21:09:50 -0700

    set the homepage styles

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,18 +1,32 @@
 import { Link } from 'react-router-dom';
 import bubbles from '../assets/ll.png';
+import cup from '../assets/lgo/cup.png';
 
 export default function Home() {
   return (
     <main>
       <div className="welcome">
-        <h1 className="playwrite-ng-modern-ll greeting mb-12">you are welcome</h1>
-        
-        <Link to="/projects" className="bubble">
-        <img src={bubbles} alt="dreamscape" className="image bubble" />
+        <div className="welcome-text">
+          <div className="welcome-title">
+          <img src={cup} alt={cup} className="image cup p-4" />
+            <h4 className="rudall my-4 p-4 flex items-baseline">you are welcome</h4>
+          </div>
+          <p className="roboto-slabby p-4 text-xs">
+            discover the creative, technical world of Adam Robson
+          </p>
+        </div>
+       
+            
+      <figure className="p-2">
+        <Link to="/projects">
+          <img src={bubbles} alt="dreamscape" className="image dreamscape" />
         </Link>
         
-        
-        <h2 className="playwrite-ng-modern-ll mt-12">by Adam Robson</h2>
+        <figcaption className="rudall mt-12 text-sm figcaption">
+          Click the image to view some of my projects, and to explore my imagination in action.
+        </figcaption>
+      </figure>
+
       </div>
     </main>
   )
